5949ec6d2e7065a4b383660b422fde45e92a09f1
penne8 <paney@post.bgu.ac.il>
2022-11-25 19:51:27 +0200

    added graph print

--- Assignment_1/graph.py
+++ Assignment_1/graph.py
@@ -33,3 +33,23 @@ class Graph(object):
         if vertex2 not in self.expand_just_vertices(vertex1) and vertex1 not in self.expand_just_vertices(vertex2):
             self.graph_dict[vertex1].append((vertex2, weight))
             self.graph_dict[vertex2].append((vertex1, weight))
+
+    def generate_edges(self):
+        edges = []
+        for vertex in self.graph_dict:
+            for neighbor_tuple in self.graph_dict[vertex]:
+                edges.append((vertex, neighbor_tuple[0], neighbor_tuple[1]))
+        return edges
+
+    def __str__(self):
+        res = "vertices: "
+        for k in self.graph_dict:
+            res += str(k) + ", "
+
+        res = res[:len(res)-2]
+        res += "\nedges: "
+        for edge in self.generate_edges():
+            res += "(" + str(edge[0].id) + ", " + str(edge[1].id) + ", " + str(edge[2]) + "), "
+        res = res[:len(res)-2]
+        return res
+
