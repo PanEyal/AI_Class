d7d0c2970043c68ccc6b9aba98cd459530cb6eb1
penne8 <paney@post.bgu.ac.il>
2022-11-26 19:04:07 +0200

    Vertex W.I.P

--- Assignment_1/vertex.py
+++ Assignment_1/vertex.py
@@ -1,19 +1,28 @@
+from enum import Enum
+
+
+class Form(Enum):
+    stable = 0
+    brittle = 1
+    broken = 2
+
+
 class Vertex(object):
-	def __init__(self, id: int, people_to_rescue: int, brittle: bool):
-		self.id = id
-		self.people_to_rescue = people_to_rescue
-		self.brittle = brittle
+    def __init__(self, id: int, people_to_rescue: int, form: Form):
+        self.id = id
+        self.people_to_rescue = people_to_rescue
+        self.form = form
 
-	def __str__(self):
-		return "[V:" + str(self.id) + ", P:" + str(self.people_to_rescue) + ", B:" + str(self.brittle) + "]"
+    def __str__(self):
+        return "[V:" + str(self.id) + ", P:" + str(self.people_to_rescue) + ", B:" + str(self.form) + "]"
 
 
-def get_vertices_list_as_string(vertices_list):
-	s = "[ "
-	for vertex in vertices_list:
-		s += str(vertex) + ", "
-	last_index_of_comma = s.rfind(",")
-	if last_index_of_comma != -1:
-		s = s[:last_index_of_comma] + s[last_index_of_comma + 1:]
+def vertex_list_to_string(vertices_list):
+    s = "[ "
+    for vertex in vertices_list:
+        s += str(vertex) + ", "
+    last_index_of_comma = s.rfind(",")
+    if last_index_of_comma != -1:
+        s = s[:last_index_of_comma] + s[last_index_of_comma + 1:]
 
-	return s + "]"
+    return s + "]"
